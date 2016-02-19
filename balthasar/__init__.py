ab8d60b32bf2a2d3b8f214054bb79957fc034d59
Olivia <dimatteo.olivia@gmail.com>
2016-02-19 10:55:33 -0500

    Wrote code to generate line operators. Still untested.

--- balthasar/__init__.py
+++ balthasar/__init__.py
@@ -2,4 +2,5 @@ from balthasar.mubs import *
 from balthasar.latinsquare import *
 from balthasar.curve import *
 from balthasar.striations import *
+from balthasar.wignerfunction import *
 
--- balthasar/wignerfunction.py
+++ balthasar/wignerfunction.py
@@ -1,4 +1,5 @@
 from pynitefields import *
+from balthasar.striations import Striations
 import numpy as np
 
 class WignerFunction():
@@ -7,17 +8,65 @@ class WignerFunction():
         Parameters:
         ===========
         field - The finite field over which the Wigner function is defined
+        mubs - The MUBs associated with this Wigner function
+        quantum_net - Association of striations to density operators
+        striations - The Striation
         point_operators - Operators at each point in discrete phase space
         matrix - The matrix which contains the probability values for each Wigner function
-        is_coarse - Optional parameter, used for specifying whether the function is coarse grained
-                    (true) or fine grained (false, i.e. a normal Wigner function)
     """
 
-    def __init__(self, field):
+    def __init__(self, mubs):
         """ Initialize the Wigner function and compute the point operators. """
+        self.field = mubs.field
+        self.mubs = mubs
+        self.striations = Striations(self.field)
+        self.quantum_net = self.compute_quantum_net()
+        #self.point_operators = compute_point_operators(self.striations, self.quantum_net)    
+
+         
+    def compute_quantum_net(self):
+        """ Compute the quantum net of the Wigner function, i.e. the association of
+            lines with quantum states (as density operators).
+
+            We associate every ray with the all +1 combination of the generators of
+            the row of MUB operators associated to that curve. 
+        """
+        # Get the table of generating operators and their matrix representation
+        gen_ops, gen_mats = self.mubs.compute_generators() 
+
+        # Horizontal shifts correspond to the Z-only operators (b = 0, so only alpha coordinate varies)
+        # Vertical shifts correspond to the X-only operators (a = 0, so only beta coordinate varies)
+        trans_hor, trans_vert = self.mubs.matrix_table[0], self.mubs.matrix_table[-1]
+        
+        quantum_net = []    
+
+        # Loop through every striation
+        for str_idx in range(len(gen_ops)):
+            next_striation_net = [] 
+            for line_idx in range(len(self.striations[0])): # For every line in the striation
+                if line_idx == 0: # If it's the ray... 
+                    gen_sum = (1.0 / self.field.dim) * np.sum(gen_mats[str_idx], 0) # Sum the generators
+                    next_striation_net.append(gen_sum)
+                else: # Otherwise, transform according to one of the translation operators
+                    transformation = np.zeros((self.field.dim, self.field.dim))
+                    if str_idx == (len(gen_ops) - 1): # Vertical striation must be handled separately and shifted using Z operators
+                        transformation = trans_hor[line_idx - 1] # -1 accounts for the fact that the identity is technically the 0th trans
+                    else: # All other striations shifted vertically horizontally using the X operators
+                        transformation = trans_vert[line_idx - 1]
+                    transformed_operator = np.dot(np.dot(transformation, next_striation_net[0]), np.asmatrix(transformation).H)
+                    next_striation_net.append(transformed_operator)
+            quantum_net.append(next_striation_net)
+                        
+        return quantum_net        
+
+    def compute_point_operators(self):
+        """ Use the quantum net and the striations to compute the point operators
+            for the Wigner function. 
+        """
+        print("Unfinished!")
 
 
-    def compute_wf(state):
+    def compute_wf(self, state):
         """ Compute the probabilities in the Wigner function for a given state.
             Input: state, a numpy array representing either a ket or a density matrix.
         """
@@ -35,7 +84,7 @@ class WignerFunction():
         return W
 
 
-    def plot(state):
+    def plot(self, state):
         """
         Plot the Wigner function of a given state.
         """
