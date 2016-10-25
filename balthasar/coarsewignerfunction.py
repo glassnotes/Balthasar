f1c1f591274e5b5dd506f699f6a079b1a4643d72
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2016-10-25 13:43:02 +0200

    Adding in what I currently have done for the coarse WF. A lot of the mechanism is there, minus some of the specifics in terms of what inputs we should use, as well as computing the new displacement operators. I really should have committed this last week when I remembered better what I was doing.

--- balthasar/coarsewignerfunction.py
+++ balthasar/coarsewignerfunction.py
@@ -31,77 +31,160 @@ class CoarseWignerFunction(WignerFunction):
         coarse_kernel - The operators at each point in the coarse phase space
     """
 
-    def __init__(self, wf, dimensions, cosets = []):
+    def __init__(self, wf, dimensions, **kwargs):
         """ Initialize a coarse-grained Wigner function based on a 
             fine-grained one.
 
+            ----- Mandatory parameters -----
             wf - A fine-grained, i.e. normal, Wigner function
+            coarse_dims - The dimensions over which to partition the
+                          field, e.g. for coarse graining dimension 16
+                          you would pass (4, 4), for 8 (2, 4), etc.
+            --------------------------------
+
+            ----- Optional parameters in kwargs -----
+            basis - Specify an alternate basis to do coarse graining over.
+                    Default is the polynomial basis.
             cosets - A set of cosets to use. Will default to a standard
-                     choice unless user provide a full set.
-                     TODO implement something that allows choice between
-                          normal/special case.
+                     choice unless user provides a full set.
+            mode - 'general' or 'subfield'. General mode works for any dim,
+                    but subfield mode works only for square dimensions. 
+            --------------------------------
         """
         # Initialize all the standard parameters
         WignerFunction.__init__(wf.mubs)
-
-        # Compute the subfield F_dimensions[0] in the big field
-
-
-        # Initialize the coset reps and compute the rest of the cosets
-        self.cosets = cosets
-
-        if len(self.cosets) == 0:
-            self.coset_reps, self.cosets = self.compute_cosets()
-        else:
-            self.coset_reps = self.cosets[0]
 
         # The dimensions of the system are as follows. The grid will be
         # partitioned into a len(cosets) x len(cosets) grid; each chunk
         # of that grid will have dimensions equal to the number of
         # elements within each coset.
-        # The effective affine space is of dimension self.coarse_dim[0]
-        self.coarse_dim = (dimensions[0], dimensions[1])
+        # The effective affine space is of dimension self.coarse_dims[0]
+        self.coarse_dims = dimensions
+        if self.coarse_dims[0] * self.coarse_dims[1] != self.dim:
+            print("Error, invalid coarse grained space dimensions.")
+            print("Dimensions must multiply to the dimension of the system.")
+            return None
+
+        # Compute the subfield F_dimensions[0] in the big field
+
+        # Loop through the optional arguments 
+        self.basis = []
+        self.coset_reps = []
+        self.cosets = []
+        self.mode = 'general'
+
+        # We will probably need to do some better validity checks here but this
+        # should be okay for now (hopefully...)
+        if kwargs is not None:
+            # Choose the basis with which to do coarse-graining (usual this 
+            # will matter only in the general case.
+            if "basis" in kwargs:
+                self.basis = kwargs["basis"]
+            else:
+                self.basis = self.compute_polynomial_basis()
+
+            if "mode" in kwargs:
+                self.mode = kwargs[mode]
+                if self.mode != "general" and self.mode != "subfield":
+                    print("Error, the mode must be 'general' or 'subfield.")
+                    return None
+                # Dimension needs to be square for subfield mode. In other
+                # words, the number of elements in each coset must be the same
+                # as the size of each coset.
+                if self.mode == "subfield":
+                    if self.dimensions[0] != self.dimensions[1]:
+                        print("Error, dim must be square for subfield mode.")
+                        return None
+
+            if "cosets" in kwargs:
+                # Make sure the cosets are valid
+                field_as_list = [f[i] for i in range(self.dim)]
+                cosets_flattened = [el for el in c for c in kwargs["cosets"]]
+                if sorted(field_as_list) != sorted(cosets_flattened):
+                    print("Error, are not a proper partitioning of the field.")
+                    return None
+                # If they're valid, choose representatives and carry on
+                self.coset_reps = [c[0] for c in cosets]
+                self.cosets = cosets
+
+        # If the cosets were not provided, compute them now. 
+        if len(self.cosets) == 0:
+            self.coset_reps, self.cosets = self.compute_cosets()
 
         # Compute the coarse displacement operators.
+        self.coarse_D = self.compute_coarse_displacement_operators()
 
         # Using the coarse displacement operators, compute the coarse kernel
         # The points here are indexed by their coset representatives.
+        self.coarse_kernel = self.compute_coarse_kernel()
+        
+
+    def compute_polynomial_basis(self):
+        # Return the polynomial basis
+        return [self.field[x] for x in range(1, self.field.n)] + [self.field[-1]]
+
 
     def compute_cosets(self):
         """ Compute a set of coset representatives if the user doesn't 
-            provide one.
+            provide one. Two cases to consider here, one where there is
+            a basis provided to make the cosets, the other where we use the
+            subfield.
         """
-        first_coset = []
 
-        # Start with the polynomial basis
-        p_basis = [self.field[x] for x in range(1, self.field.n)] + \
-                     [self.field[-1]] # Make sure to add element 1
-    
-        # Compute all linear combinations of all but 1 using the subfield 
-        # First generate all combinations of the coefficients
-        coefficient_lists = product(self.subfield, repeat = self.field.n - 1) 
-        for coeffs in coefficient_list:
-            linear_comb = [coeffs[i] * p_basis[i] for i in range(len(coeffs))]
-            s = 0
-            for el in linear_comb:
-                s += el
-            first_coset.append(s)
+        if self.mode == "general":
+            # In the general mode, for a system p^mn, we can really only take
+            # out one copy of p in the most general case by using this method.
+            first_coset = []
 
-        # Use the remaining elements
+            # Compute all linear combinations of all but 1 using the subfield 
+            # First generate all combinations of the coefficients
+            coefficient_lists = product(self.subfield, repeat = self.field.n - 1) 
+            for coeffs in coefficient_list:
+                l_comb = [coeffs[i] * self.basis[i] for i in range(len(coeffs))]
+                s = self.field[0]
+                for el in l_comb:
+                    s += el
+                first_coset.append(s)
 
-        return first_coset
+            # Use the remaining element 
 
-    def compute_cosets(self):
-        """ Compute the cosets based on the coset representatives.
+            return first_coset
+        else:
+            return None
+
+    def compute_coarse_D(self):
+
+
+
+
+    def compute_coarse_kernel(self):
+        """ Compute the kernel of the coarse-grained Wigner function. This
+            function is pretty much the same as the one for the normal WF, 
+            but we need to index the kernel by coset representatives rather
+            than plain old field elements this time.
+
+            Returns a dictionary mapping a pair of coset reps (a, b) to w(a,b).
         """
-        # The number of coset reps should divide the dimension perfectly.
-        # Of course there is more to it than this, but we can handle it later
-        if self.dim % len(self.coset_reps) != 0:
-            print("Error, invalid coset representatives.")
-            return 
 
-        
+        ckernel = {}
 
+        ckernel_00 = np.zeros((self.dim, self.dim), dtype = np.complex_)
+        if self.field.p != 2:
+            for key in self.coarse_D.keys():
+                ckernel_00 = ckernel_00 + \
+                    (self.coarse_D[key][0].eval() * self.coarse_D[key][1]
+            ckernel_00 = ckernel_00 / self.dim
+            ckernel[(self.coset_reps[0], self.coset_reps[0])] = ckernel_00
+        else:
+            if self.field.n == 1:
+                for key in self.coarse_D.keys():
+                    ckernel_00 = ckernel_00 + \
+                        (self.coarse_D[key][0] * self.coarse_D[key][1])
+                ckernel_00 = ckernel_00 /self.dim
+                ckernel[(self.coset_reps[0], self.coset_reps[0])] = ckernel_00
+
+        return ckernel
+                
 
 
     def compute_wf(self, state):                                                
